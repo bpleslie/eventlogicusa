b685a6aa1edbf5fc9b1c6e957a31f9b95a17e35e
bpleslie <bradleypleslie@gmail.com>
2018-07-30 19:48:17 -0700

    accessibility changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@
 			// Navigation Button.
 				$(
 					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
+						'<a href="#navPanel" class="toggle" title="Menu"></a>' +
 					'</div>'
 				)
 					.appendTo($body);
